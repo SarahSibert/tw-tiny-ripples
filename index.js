52b447a3ffd59b1c316c0bdd8f5d4ddb9ab09ffe
SarahSibert <mail@sarahsibert.com>
2021-10-21 09:30:53 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = require("tailwindcss/plugin")(
+    function ({}) {
+        }, 
+        { 
+        theme: {
+            extend: {
+                colors: {
+                    'forest-green' : '#10564F',
+                    'blue-green' : '#21B6A8',
+                    'aqua' : '#30D9C8',
+                    'tiffany-blue' : '#75E6DA',
+                },
+            }
+        }
+    }
+);
